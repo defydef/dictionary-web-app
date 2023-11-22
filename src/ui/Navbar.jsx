8db9cdd7b784650fabcaa55df467f87b0df90f52
defy <8841170+defydef@users.noreply.github.com>
2023-11-22 20:55:50 +0700

    Finish Navbar for mobile view

--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -5,8 +5,19 @@ function Navbar() {
         <li>
           <img src="./images/logo.svg" alt="Dictionary logo" />
         </li>
-        <li>
+        <li className="flex gap-3 justify-between items-center">
           <p className="text-[0.875rem] font-bold leading-6">Mono</p>
+          <span className=" cursor-pointer">
+            <img src="./images/icon-arrow-down.svg" alt="dropdown arrow" />
+          </span>
+          <div className="w-[0.0625rem] h-8 bg-[var(--grey)]"></div>
+          <label className="switch">
+            <input type="checkbox" />
+            <span className="slider round"></span>
+          </label>
+          <span className=" cursor-pointer">
+            <img src="./images/icon-moon.svg" alt="dark mode toggle" />
+          </span>
         </li>
       </ul>
     </nav>
