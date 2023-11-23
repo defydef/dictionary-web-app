f2b66da109a2d2132d654d1eb7733dbaf2abc2db
defy <8841170+defydef@users.noreply.github.com>
2023-11-23 07:43:57 +0700

    Create input field for mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Container from "./ui/Container";
 import Navbar from "./ui/Navbar";
+import SearchInput from "./ui/SearchInput";
 
 function App() {
   return (
     <Container>
       <Navbar />
+      <SearchInput />
     </Container>
   );
 }
--- src/ui/Container.jsx
+++ src/ui/Container.jsx
@@ -1,5 +1,7 @@
 function Container({ children }) {
-  return <div className="p-6">{children}</div>;
+  return (
+    <div className="p-6 flex flex-col gap-6 justify-center">{children}</div>
+  );
 }
 
 export default Container;
