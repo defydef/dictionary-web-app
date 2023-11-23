1d4c1114c89a8adf835620bba1b5ab048631589b
defy <8841170+defydef@users.noreply.github.com>
2023-11-23 08:16:41 +0700

    Change border color of input-parent into purple, when the input is in focus

--- src/ui/Container.jsx
+++ src/ui/Container.jsx
@@ -1,6 +1,8 @@
 function Container({ children }) {
   return (
-    <div className="p-6 flex flex-col gap-6 justify-center">{children}</div>
+    <div className="p-6 flex flex-col gap-6 justify-center font-[Inconsolata] font-bold text-base">
+      {children}
+    </div>
   );
 }
 
--- src/ui/SearchInput.jsx
+++ src/ui/SearchInput.jsx
@@ -1,14 +1,16 @@
 function SearchInput() {
   return (
     <div className="grid grid-cols-8 grid-rows-1">
-      <input
-        type="text"
-        className="bg-[var(--light-grey-2)] row-start-1 col-start-1 col-span-full h-12 rounded-2xl pl-5"
-      />
-      <img
-        src="./images/icon-search.svg"
-        className="col-start-8 col-span-1 row-start-1 my-auto"
-      />
+      <div className="active:border border-solid border-[var(--purple)] bg-[var(--light-grey-2)] row-start-1 col-start-1 col-span-full h-12 rounded-2xl pl-5 grid grid-cols-8 input-parent">
+        <input
+          type="text"
+          className=" bg-transparent row-start-1 col-start-1 col-span-7 z-10 pr-3 focus:outline-none"
+        />
+      </div>
+
+      <span className="col-start-8 col-span-1 row-start-1 my-auto z-10">
+        <img src="./images/icon-search.svg" />
+      </span>
     </div>
   );
 }
